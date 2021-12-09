8fd395b8791605c171285c6efe952bf558e1f741
Josie DeRosier <s209055@students.aie.edu>
2021-12-08 17:47:37 -0800

    Messing with Naughty Attributes and testing different types of cycles

---
+++ Assets/Scripts/NaughtyComponent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class NaughtyComponent : MonoBehaviour
+{
+    [Dropdown("StringValues")]
+    public string stringValue;
+
+    private List<string> StringValues { get { return new List<string>() { "Go Until", "Back and Forth" }; } }   // TODO* add any other types of cycles here
+}
--- Assets/Scripts/TimedAspect.cs
+++ Assets/Scripts/TimedAspect.cs
@@ -8,22 +8,34 @@ public class TimedAspect : MonoBehaviour
     public float currentCyclePercent;   // the current percentage of how far through the cycle the aspect is (0 = start, 1 = end)
 
     public bool cycleCompleted;         // has the cycle completed? on a back and forth, this will shift between true and false when the cycle turns over
+    [Tooltip("How many hours until a cycle change")]
     public float cycleSpeed;            // how many hours until a cycle change
 
     public bool hasSlider;              // will this aspect use a UI slider?
     public Slider currentPercentSlider; // Unity UI slider to show the current percentage of the cycle completed (will go up and down on a back and forth aspect)
 
-
+    public NaughtyComponent dropdown;
     public int cycleType;       // 0 is no cycle, 1 is Go Until, 2 is Back and Forth
 
     void Start()
     {
         currentCyclePercent = 0;
         cycleCompleted = false;
+        // dropdown.stringValue = "Go Until";
     }
 
-    void Update()
+    private void Update()
     {
-        
+        switch (dropdown.stringValue)
+        {
+            case "Go Until":
+                cycleType = 1;
+                break;
+            case "Back and Forth":
+                cycleType = 2;
+                break;
+            default:
+                break;
+        }
     }
 }
